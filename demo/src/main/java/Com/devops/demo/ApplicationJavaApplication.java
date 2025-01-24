9a21ebe7885043f10fcf811bb7d004248089e3de
ahmat <ahmatnassour58@gmail.com>
2025-01-24 07:57:38 -0800

    creation de l'application java

---
+++ demo/src/main/java/Com/devops/demo/ApplicationJavaApplication.java
@@ -0,0 +1,13 @@
+package Com.devops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationJavaApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/Com/devops/demo/ApplicationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.devops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
